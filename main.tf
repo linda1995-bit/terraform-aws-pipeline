68bb6028c0a7da8ea7f07e4f8cf4c546629e2a51
linda <lindajoseph1995@gmail.com>
2026-09-06 15:35:05 +0100

    Test branch protection

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "github-terraform-vpc-test-test"
-  }
+  Name        = "github-terraform-vpc-test"
+  Environment = "dev"
+}
 }
